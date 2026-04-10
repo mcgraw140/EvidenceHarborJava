ffa026921f6a8501e19ea5304ca0c2876de978bc
camde <camde@users.noreply.github.com>
2026-04-10 14:47:54 -0500

    Fix launcher execution path for folder run icon

--- Run-EvidenceHarbor.bat
+++ Run-EvidenceHarbor.bat
@@ -2,36 +2,33 @@
 setlocal
 
 set "APP_DIR=%~dp0"
-set "JAR_PATH=%APP_DIR%target\evidence-harbor-1.0.0.jar"
-set "JAVA_EXE=javaw"
 set "MAVEN_EXE=C:\Program Files\Maven\apache-maven-3.9.9\bin\mvn.cmd"
 set "JDK_HOME=C:\Program Files\Microsoft\jdk-21.0.10.7-hotspot"
+set "JAVA_HOME=%JDK_HOME%"
 
-if exist "C:\Program Files\Microsoft\jdk-21.0.10.7-hotspot\bin\javaw.exe" (
-  set "JAVA_EXE=C:\Program Files\Microsoft\jdk-21.0.10.7-hotspot\bin\javaw.exe"
-  set "JAVA_HOME=%JDK_HOME%"
-)
-
-if not exist "%JAR_PATH%" (
-  echo First-time setup: building app jar...
-  if not exist "%MAVEN_EXE%" (
-    echo Maven not found at: %MAVEN_EXE%
-    exit /b 1
-  )
-  pushd "%APP_DIR%"
-  call "%MAVEN_EXE%" -q -DskipTests package
-  set "BUILD_EXIT=%ERRORLEVEL%"
-  popd
-  if not "%BUILD_EXIT%"=="0" (
-    echo Build failed.
-    exit /b 1
-  )
-)
-
-if not exist "%JAR_PATH%" (
-  echo Could not find jar: %JAR_PATH%
+if not exist "%JAVA_HOME%\bin\java.exe" (
+  echo JDK not found at: %JAVA_HOME%
+  echo Please install Java 21.
+  pause
   exit /b 1
 )
 
-start "EvidenceHarbor" "%JAVA_EXE%" -jar "%JAR_PATH%"
+if not exist "%MAVEN_EXE%" (
+  echo Maven not found at: %MAVEN_EXE%
+  echo Please install Maven 3.9.9 or update this launcher path.
+  pause
+  exit /b 1
+)
+
+pushd "%APP_DIR%"
+call "%MAVEN_EXE%" -q -DskipTests javafx:run
+set "RUN_EXIT=%ERRORLEVEL%"
+popd
+
+if not "%RUN_EXIT%"=="0" (
+  echo Application failed to start. Exit code: %RUN_EXIT%
+  pause
+  exit /b %RUN_EXIT%
+)
+
 exit /b 0
